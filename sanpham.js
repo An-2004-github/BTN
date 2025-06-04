00cfc6b1c94f57f280e0bf019d5ec6b70f53f1d7
ThinhdanghocBE <vuthinh122004@gmail.com>
2025-06-05 02:32:19 +0700

    sửa lại trang chi tiết sản phẩm

---
+++ sanpham.js
@@ -0,0 +1,29 @@
+const Mac = [
+    {
+            id: 1,
+            name: "iMac 24 inch M4 2024",
+            image: "../../images/Logo/apple image/iMac 24 inch M4 2024.jpg",
+            installment: "Trả góp 0%",
+            priceOld: 39990000,
+            discountPercent: 1,
+            priceNew: 39600000,
+        },
+        {
+            id: 2,
+            name: "iMAC 24i 2023 M3",
+            image: "../../images/Logo/apple image/iMAC 24i 2023 M3.png",
+            installment: "Trả góp 0%",
+            priceOld: 39990000,
+            discountPercent: 1,
+            priceNew: 39600000,
+        },
+        {
+            id: 3,
+            name: "Mac mini M4 2024",
+            image: "../../images/Logo/apple image/Mac mini M4 2024.jpeg",
+            installment: "Trả góp 0%",
+            priceOld: 14990000,
+            discountPercent: 5,
+            priceNew: 14240500,
+        },
+]
